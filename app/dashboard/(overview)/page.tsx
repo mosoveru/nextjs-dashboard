a1f84352113d15c39a7db96b301cdfd050503201
Mosoveru <mosoveru@gmail.com>
2025-03-22 12:52:42 +0300

    feat: add error handling

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,11 @@
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import {LatestInvoicesSkeleton, RevenueChartSkeleton, CardSkeleton} from "@/app/ui/skeletons";
 import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
